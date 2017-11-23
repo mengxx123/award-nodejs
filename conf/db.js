d8d75c87cf3000152b67d11a7f32e865ac4bcd50
gitcjh <1418503647@.qq.com>
2017-11-23 11:42:25 +0800

    修改配置文件

--- conf/db.js
+++ conf/db.js
@@ -3,7 +3,7 @@
 module.exports = {
     mysql: {
         host: 'localhost', 
-        user: 'root',
+        user: 'root2',
         password: '123456',
         database:'ys_sign', // 前面建的user表位于些数据库中
         port: 3306,
